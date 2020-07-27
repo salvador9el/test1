b4b679f02a4b7efd87da1d22c40c6a2da99118cf
salvador9el <68823017+salvador9el@users.noreply.github.com>
2020-07-27 13:50:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,5 @@
 const Discord = require("discord.js");
-const myid = ['ID'];
+const myid = ['736563409281220608'];
 const prefix = ['$'];
 const client = new Discord.Client();
 const client2 = new Discord.Client();
